2db3c9034db2e6b4f518274b15093567cf20ee18
FaultyRAM <explodingpsu@gmail.com>
2015-11-19 11:12:48 +1000

    Parse command line args via docopt

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,66 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+extern crate docopt;
+use docopt::Docopt;
+use docopt::Error as DocoptError;
+
+extern crate rustc_serialize;
+
+use std::io::{Write, stderr};
+
+const USAGE: &'static str = "
+Usage:
+	ng-stats local [-fV] <src-log-dir> <dest-log-dir> <src-html-dir> <dest-html-dir> <db-dir>
+	ng-stats world [-V]  <src-log-dir> <dest-url>
+	ng-stats (--help | --version)
+
+Options:
+	-f, --force    Rebuild the local database
+	-V, --verbose  Print verbose information to stdout
+	--help         Display this help message and exit
+	--version      Display version information and exit
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+	arg_src_log_dir:   Option<String>,
+	arg_dest_log_dir:  Option<String>,
+	arg_src_html_dir:  Option<String>,
+	arg_dest_html_dir: Option<String>,
+	arg_db_dir:        Option<String>,
+	arg_dest_url:      Option<String>,
+	cmd_local:         bool,
+	cmd_world:         bool,
+	flag_force:        bool,
+	flag_verbose:      bool,
+	flag_help:         bool,
+	flag_version:      bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let parse_result = Docopt::new(USAGE).and_then(|d| {
+		d.help(true).version(Some(String::from("ng-stats 0.1.0"))).decode()
+	});
+	if parse_result.is_err() {
+		let e = parse_result.unwrap_err();
+		match e {
+			DocoptError::Help | DocoptError::Version(_) => {
+				println!("{}", e);
+			},
+			_ => {
+				let mut stderr = stderr();
+				writeln!(&mut stderr, "{}", e).unwrap();
+			},
+		}
+		return;
+	};
+	let args: Args = parse_result.unwrap();
+	if args.cmd_local {
+		unimplemented!()
+	}
+	if args.cmd_world {
+		unimplemented!()
+	}
+	unreachable!()
 }
